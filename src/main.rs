f4a45b703579da04283117af81d818163d607b88
= <=>
2024-03-17 16:52:38 +0100

    extract logger

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,24 @@
 mod glfw_egui;
+mod logger;
 mod plot;
 
+mod audio;
 mod audio_analyzer;
 mod audio_processor;
+use audio_processor::AudioProcessor;
+
 pub mod ui_helpers;
 use std::time::{Duration, Instant};
-
-use audio_processor::AudioProcessor;
-mod audio;
 
 const SCREEN_WIDTH: u32 = 1920;
 const SCREEN_HEIGHT: u32 = 1080;
 const DEFAULT_RESOLUTION: (u32, u32) = (SCREEN_WIDTH, SCREEN_HEIGHT);
 
-use log::{Level, Metadata, Record};
-
-struct SimpleLogger;
-
-impl log::Log for SimpleLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Trace
-    }
-
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!(
-                "{} | {} | {}",
-                record.level(),
-                record.target(),
-                record.args()
-            );
-        }
-    }
-
-    fn flush(&self) {}
-}
-
-static LOGGER: SimpleLogger = SimpleLogger;
-
 fn main() {
     println!("Hello RtAudioEffect!");
 
-    if let Err(err) = log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::Debug))
+    if let Err(err) =
+        log::set_logger(&logger::LOGGER).map(|()| log::set_max_level(log::LevelFilter::Debug))
     {
         eprintln!("log::set_logger failed: {err:#?}");
     }
