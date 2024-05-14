62863bfd70a9ceac49d338ccff9fa20ca9523949
walkiewicz125 <=>
2024-05-14 19:47:19 +0200

    poc

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ mod ui;
 use egui_glfw::AppWindow;
 use log::{error, info};
 use mdns_sd::{ServiceDaemon, ServiceInfo};
+use serializer::*;
+use serializer_macro::Serializable;
 
 use std::{
+    default,
+    mem::size_of,
+    net::UdpSocket,
     sync::{Arc, Mutex},
     thread,
     time::Duration,
@@ -136,22 +141,65 @@ impl AppContext {
     }
 }
 
+pub mod serializer;
+pub use serializer::*;
+extern crate serializer_macro;
+trait Serializable {
+    fn get_bytes(&self) -> Vec<u8>;
+    fn from_bytes(&mut self, bytes: &[u8]) -> u32;
+}
+
+#[derive(Serializable, Default)]
+struct Structa {
+    dupa: u32,
+    asss: u32,
+    cos: String,
+}
+
+#[derive(Serializable, Default)]
+struct Structaa {
+    dupa: u32,
+    asss: f32,
+    cos: String,
+    s: Structa,
+}
+
 fn main() {
     info!("Hello RtAudioEffect!");
+    // let mut serializer = Serializer::new();
+    // serializer.serialize(Packet::new().add(1).add(&2));
+
+    let mut a = Structaa {
+        dupa: 42,
+        asss: 3.14,
+        cos: String::from("dupa"),
+        s: Structa {
+            dupa: 42,
+            asss: 3,
+            cos: String::from("dupa"),
+        },
+    }
+    .get_bytes();
+    let mut b = Structaa::default();
+    b.from_bytes(&a);
 
     if let Err(err) =
-        log::set_logger(&logger::LOGGER).map(|()| log::set_max_level(log::LevelFilter::Debug))
+        log::set_logger(&logger::LOGGER).map(|()| log::set_max_level(log::LevelFilter::Info))
     {
         eprintln!("log::set_logger failed: {err:#?}");
     }
 
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind UDP socket");
+    println!("Socket address: {:?}", socket.local_addr().unwrap());
+
+    let port = socket.local_addr().unwrap().port();
     let mdns = ServiceDaemon::new().expect("Failed to create mDNS daemon");
     let rt_audio_efect_service = ServiceInfo::new(
         "_RtAudioEffect._udp.local.",
         "RtAudioEffect",
         "RtAudioEffect.local.",
         "127.0.0.1",
-        12337,
+        port,
         None,
     )
     .unwrap()
@@ -161,6 +209,21 @@ fn main() {
         .expect("Failed to register RtAudioEffect service in mDNS deamon");
 
     let mut context = AppContext::new();
+    let udp_thread = thread::spawn(move || {
+        let mut buffer = [0; 1024];
+        loop {
+            println!("Waiting for data...");
+            let (amt, src) = socket
+                .recv_from(&mut buffer)
+                .expect("Failed to receive data from UDP socket");
+            info!("Received {amt} bytes from {src}");
+            let mut buffer = &mut buffer[..amt];
+            buffer.reverse();
+            socket
+                .send_to(buffer, &src)
+                .expect("Failed to send data to UDP socket");
+        }
+    });
 
     if context.run() {
         info!("RtAudioEffect exit successfully");
