e30a9e499c4a39380d27dac1075da06456bc3b68
walkiewicz125 <=>
2024-07-20 06:01:41 +0200

    swap serialziers

--- serializer/serializer_macro/src/lib.rs
+++ serializer/serializer_macro/src/lib.rs
@@ -1,79 +1,318 @@
 extern crate proc_macro;
 
-use std::any::Any;
-
-use quote::quote;
-use syn;
-
 #[proc_macro_derive(Serializable)]
 pub fn serializable_macro_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse(input).unwrap();
 
     // Build the trait implementation
-    impl_serializable_macro(&ast)
+    serializable_impl::impl_serializable_macro(&ast)
 }
 
-fn impl_serializable_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
-    let name = &ast.ident;
+mod serializable_impl {
+    extern crate proc_macro2;
+    use std::mem::Discriminant;
 
-    let mut serializing_fields = Vec::new();
-    let mut deserializing_fields = Vec::new();
+    use proc_macro2::TokenStream;
+    use quote::quote;
+    use syn::{spanned::Spanned, Error};
 
-    if let syn::Data::Struct(data_struct) = &ast.data {
-        if let syn::Fields::Named(named) = &data_struct.fields {
-            for field in named.named.iter() {
-                let field_name = &field.ident.clone().unwrap();
-                let field_serialization = quote! {
-                    data.extend_from_slice(self.#field_name.get_bytes().as_slice());
-                };
-                serializing_fields.push(field_serialization);
+    pub fn impl_serializable_macro(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
+        let name = &ast.ident;
 
-                let field_deserialization = quote! {
-                    let field_size = self.#field_name.from_bytes(&bytes[ind as usize..]);
-                    size += field_size;
-                    ind += field_size;
-                };
+        let mut code = SerializingCode::empty();
 
-                deserializing_fields.push(field_deserialization);
+        match &ast.data {
+            syn::Data::Struct(data_struct) => match impl_struct(data_struct) {
+                Ok(serializing_code) => {
+                    code = serializing_code;
+                }
+                Err(e) => {
+                    return e.into_compile_error().into();
+                }
+            },
+            syn::Data::Enum(data_enum) => match impl_enum(name, data_enum) {
+                Ok(serializing_code) => {
+                    code = serializing_code;
+                }
+                Err(e) => {
+                    return e.into_compile_error().into();
+                }
+            },
+            syn::Data::Union(data_union) => {
+                panic!("Unions are not supported");
+            }
+        }
 
-                println!("Field name: {:?}", field_name);
+        let serializing_fields = code.serialize;
+        let deserializing_fields = code.deserialize;
+
+        let generator = quote! {
+            impl Serializable for #name {
+                fn to_bytes(&self) -> Vec<u8> {
+                    let mut data = Vec::<u8>::new();
+                    #(#serializing_fields)*
+                    data
+                }
+
+                fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<u32, String>{
+                    struct ByteReader {
+                        bytes: Vec<u8>,
+                        offset: usize,
+                    }
+
+                    impl ByteReader {
+                        fn new(bytes: Vec<u8>) -> Self {
+                            ByteReader { bytes, offset: 0 }
+                        }
+
+                        fn advance(&mut self, size: usize) {
+                            self.offset += size;
+                        }
+
+                        fn peek(&self) -> &[u8] {
+                            &self.bytes[self.offset..]
+                        }
+
+                        fn are_all_bytes_consumed(&self) -> bool {
+                            self.offset == self.bytes.len()
+                        }
+
+                        fn bytes_consumed(&self) -> usize {
+                            self.offset
+                        }
+
+                        fn length(&self) -> usize {
+                            self.bytes.len()
+                        }
+                    }
+
+                    let mut reader = ByteReader::new(bytes);
+
+                    #(#deserializing_fields)*
+
+                    if ! reader.are_all_bytes_consumed() {
+                        return Err(format!("Not all bytes were consumed. Consumed: {}, Total: {}", reader.bytes_consumed(), reader.length()));
+                    }
+
+                    Ok(reader.offset as u32)
+                }
+            }
+        };
+        // print!("Generated impl: {}", quote! { #generator });
+        generator.into()
+    }
+
+    struct SerializingCode {
+        serialize: Vec<TokenStream>,
+        deserialize: Vec<TokenStream>,
+    }
+
+    impl SerializingCode {
+        fn empty() -> Self {
+            Self {
+                serialize: Vec::new(),
+                deserialize: Vec::new(),
             }
         }
     }
 
-    let gen = quote! {
-        impl Serializable for #name {
-            fn get_bytes(&self) -> Vec<u8> {
-                let mut data = Vec::<u8>::new();
-                #(#serializing_fields)*
-                println!("Serialized data: {:?}", data);
-                data
+    fn impl_struct(data_struct: &syn::DataStruct) -> Result<SerializingCode, Error> {
+        let mut code = SerializingCode::empty();
+
+        if let syn::Fields::Named(named) = &data_struct.fields {
+            for field in named.named.iter() {
+                let field_name = &field.ident.clone().unwrap();
+
+                let field_serialization = quote! {
+                    data.extend_from_slice(self.#field_name.to_bytes().as_slice());
+                };
+                code.serialize.push(field_serialization);
+
+                let field_deserialization = quote! {
+                    let size = self.#field_name.from_bytes(reader.peek().to_vec())?;
+                    reader.advance(size as usize);
+                };
+                code.deserialize.push(field_deserialization);
+            }
+            return Ok(code);
+        } else {
+            return Err(Error::new(
+                data_struct.fields.span(),
+                "Only named fields are supported",
+            ));
+        }
+    }
+
+    fn impl_enum(
+        enum_name: &syn::Ident,
+        data_enum: &syn::DataEnum,
+    ) -> Result<SerializingCode, Error> {
+        let mut code = SerializingCode::empty();
+
+        for (discriminator_usize, variant) in data_enum.variants.iter().enumerate() {
+            let discriminator = discriminator_usize as u32;
+            let variant_name = &variant.ident;
+
+            if let Some(_) = &variant.discriminant {
+                return Err(Error::new(
+                    variant.span(),
+                    "Discriminants are not supported",
+                ));
             }
 
-            fn from_bytes(&mut self, bytes: &[u8]) -> u32 {
-                let mut ind: u32 = 0;
-                let mut size: u32 = 0;
-                #(#deserializing_fields)*
+            match &variant.fields {
+                syn::Fields::Named(named) => {
+                    let mut fields_names = Vec::<TokenStream>::new();
+                    let mut inner_code_serializing = Vec::<TokenStream>::new();
+                    let mut inner_code_deserializing = Vec::<TokenStream>::new();
+                    for field in named.named.iter() {
+                        let field_name;
+                        if let Some(field_name_ident) = &field.ident {
+                            field_name = field_name_ident;
+                        } else {
+                            return Err(Error::new(field.span(), "Named fields are required"));
+                        }
 
-                size
-            }
+                        fields_names.push(quote! { #field_name });
+                        inner_code_serializing.push(quote! {
+                            data.extend_from_slice(#field_name.to_bytes().as_slice());
+                        });
+                        inner_code_deserializing.push(quote! {#field_name:{
+                            let mut #field = Default::default();
+                            let size = #field_name.from_bytes(reader.peek().to_vec())?;
+                            reader.advance(size as usize);
+                            #field_name}
+                        });
 
-            fn try_from_bytes(bytes: &[u8]) -> Result<Self, String> {
-                let mut instance = Self::default();
-                instance.from_bytes(bytes);
+                        println!(
+                            "Filed serialization named: <<{}>>",
+                            inner_code_serializing.last().unwrap()
+                        );
+                        println!(
+                            "Filed deserialization named: <<{}>>",
+                            inner_code_deserializing.last().unwrap()
+                        );
+                    }
+                    code.serialize.push(quote! {
+                        #enum_name::#variant_name{#(#fields_names),*} => {
+                            let value = &(#discriminator as u32).to_ne_bytes();
+                            data.extend_from_slice(value);
+                            #(#inner_code_serializing)*
+                        }
+                    });
+                    code.deserialize.push(quote! {
+                        #discriminator => {
+                            *self = #enum_name::#variant_name{
+                                #(#inner_code_deserializing),*
+                            }
+                        }
+                    });
 
-                Ok(instance)
+                    println!(
+                        "code.serialize named: <<{}>>",
+                        code.serialize.last().unwrap()
+                    );
+                    println!(
+                        "code.deserialize named: <<{}>>",
+                        code.deserialize.last().unwrap()
+                    );
+                }
+                syn::Fields::Unnamed(unnamed) => {
+                    let mut fields_names = Vec::<TokenStream>::new();
+                    let mut inner_code_serializing = Vec::<TokenStream>::new();
+                    let mut inner_code_deserializing = Vec::<TokenStream>::new();
+                    for (field_index, field) in unnamed.unnamed.iter().enumerate() {
+                        let field_name =
+                            syn::Ident::new(&format!("field_{}", field_index), field.span());
+                        fields_names.push(quote! { #field_name });
+
+                        inner_code_serializing.push(quote! {
+                            data.extend_from_slice(#field_name.to_bytes().as_slice());
+                        });
+                        inner_code_deserializing.push(quote! {{
+                            let mut field : #field = Default::default();
+                            let size = field.from_bytes(reader.peek().to_vec())?;
+                            reader.advance(size as usize);
+                            field}
+                        });
+
+                        println!(
+                            "Filed serialization unnamed: <<{}>>",
+                            inner_code_serializing.last().unwrap()
+                        );
+                        println!(
+                            "Filed deserialization unnamed: <<{}>>",
+                            inner_code_deserializing.last().unwrap()
+                        );
+                    }
+                    code.serialize.push(quote! {
+                        #enum_name::#variant_name(#(#fields_names),*) => {
+                            let value = &(#discriminator as u32).to_ne_bytes();
+                            data.extend_from_slice(value);
+                            #(#inner_code_serializing)*
+                        }
+                    });
+                    code.deserialize.push(quote! {
+                        #discriminator => {
+                            *self = #enum_name::#variant_name(
+                                #(#inner_code_deserializing),*
+                            )
+                        }
+                    });
+
+                    println!(
+                        "code.serialize unnamed: <<{}>>",
+                        code.serialize.last().unwrap()
+                    );
+                    println!(
+                        "code.deserialize unnamed: <<{}>>",
+                        code.deserialize.last().unwrap()
+                    );
+                }
+                syn::Fields::Unit => {
+                    code.serialize.push(quote! {
+                        #enum_name::#variant_name => {
+                            let value = &(#discriminator as u32).to_ne_bytes();
+                            data.extend_from_slice(value);
+                        }
+                    });
+                    code.deserialize.push(quote! {
+                        #discriminator => {
+                            *self = #enum_name::#variant_name;
+                        }
+                    });
+                }
             }
         }
 
-        impl From<&[u8]> for #name {
-            fn from(bytes: &[u8]) -> Self {
-                let mut instance = Self::default();
-                instance.from_bytes(bytes);
-                instance
-            }
-        }
-    };
+        let enum_field_serialization = code.serialize;
+        let enum_fields_deserialization = code.deserialize;
 
-    gen.into()
+        let enum_serialization = quote! {
+            match self {
+                #(#enum_field_serialization)*
+            }
+        };
+
+        let enum_deserialization = quote! {
+            let discriminator = u32::from_ne_bytes(
+                reader.peek()[0..4]
+                    .try_into()
+                    .map_err(|e| format!("Error parsing discriminator: {}", e))?,
+            );
+            reader.advance(4);
+
+            match discriminator {
+                #(#enum_fields_deserialization)*
+                _ => {
+                    return Err(format!("Invalid discriminator: {}", discriminator));
+                }
+            }
+        };
+
+        return Ok(SerializingCode {
+            serialize: vec![enum_serialization],
+            deserialize: vec![enum_deserialization],
+        });
+    }
 }
--- serializer/src/lib.rs
+++ serializer/src/lib.rs
@@ -1,123 +1,60 @@
-use log::error;
 extern crate serializer_macro;
+
 pub use serializer_macro::Serializable;
 
 pub trait Serializable {
-    fn try_from_bytes(bytes: &[u8]) -> Result<Self, String>
+    fn to_bytes(&self) -> Vec<u8>;
+    fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<u32, String>
     where
         Self: Sized;
-    fn get_bytes(&self) -> Vec<u8>;
-    fn from_bytes(&mut self, bytes: &[u8]) -> u32;
 }
 
-macro_rules! impl_packet_buildable_for_trivial {
+impl Serializable for String {
+    // The first 4 bytes are the length of the string
+    // The rest of the bytes are the string itself
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = (self.len() as u32).to_ne_bytes().to_vec();
+        bytes.extend_from_slice(self.as_bytes());
+        bytes
+    }
+
+    fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<u32, String> {
+        if bytes.len() < 4 {
+            return Err(format!(
+                "Invalid length of bytes. Expected at least 4, got: {}",
+                bytes.len(),
+            ));
+        }
+        let size = u32::from_ne_bytes(bytes[0..4].try_into().map_err(|e| format!("Error parsing bytes: {}", e))?);
+        *self = String::from_utf8(bytes[4..4+size as usize].to_vec())
+            .map_err(|e| format!("Error parsing string: {}", e))?;
+        Ok(4 + self.len() as u32)
+    }
+}
+
+macro_rules! impl_serializable_for_trivial {
     ($($t:ty),*) => {
         $(
             impl Serializable for $t {
-                fn get_bytes(&self) -> Vec<u8> {
+                fn to_bytes(&self) -> Vec<u8> {
                     self.to_ne_bytes().to_vec()
                 }
 
-                fn from_bytes(&mut self, bytes: &[u8]) -> u32 {
+                fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<u32, String> {
+                    if bytes.len() != std::mem::size_of::<Self>() {
+
+                    }
                     let size = std::mem::size_of::<Self>();
-                    *self = Self::from_ne_bytes(bytes[0..size].try_into().unwrap());
-                    size as u32
-                }
-
-                fn try_from_bytes(bytes: &[u8]) -> Result<Self, String> {
-                    let mut instance = Self::default();
-                    instance.from_bytes(bytes);
-
-                    Ok(instance)
+                    *self = <$t>::from_ne_bytes(bytes[0..size].try_into().map_err(|e| format!("Error parsing bytes: {}", e))?);
+                    Ok(size as u32)
                 }
             }
         )*
     };
 }
 
-impl_packet_buildable_for_trivial!(i8, u8, i16, u16, i32, u32, i64, u64, f32, f64);
-
-impl Serializable for String {
-    // [len: u32:NE; data: [u8]]
-
-    fn get_bytes(&self) -> Vec<u8> {
-        let mut data = (self.len() as u32).to_ne_bytes().to_vec();
-        data.extend_from_slice(self.as_bytes());
-
-        data
-    }
-
-    fn from_bytes(&mut self, bytes: &[u8]) -> u32 {
-        let len = u32::from_ne_bytes(bytes[0..4].try_into().unwrap()) as usize;
-        let bytes = &bytes[4..4 + len];
-        match String::from_utf8(bytes.to_vec()) {
-            Ok(str) => *self = str,
-            Err(err) => {
-                *self = String::from("");
-                error!("Error while converting bytes to string: {:?}", err);
-            }
-        }
-
-        bytes.len() as u32
-    }
-    fn try_from_bytes(bytes: &[u8]) -> Result<Self, String> {
-        let mut instance = String::from("");
-        instance.from_bytes(bytes);
-
-        Ok(instance)
-    }
-}
+impl_serializable_for_trivial!(i8, u8, i16, u16, i32, u32, i64, u64, f32, f64);
 
 #[cfg(test)]
-mod tests {
-    use crate::Serializable;
-    #[test]
-    fn test_string() {
-        let s = String::from("Hello, World!");
-
-        let bytes = s.get_bytes();
-        let mut s2 = String::from("");
-        s2.from_bytes(&bytes);
-
-        assert_eq!(s, s2);
-    }
-
-    // generate tests for types: i8, u8, i16, u16, i32, u32, i64, u64, f32, f64
-    macro_rules! test_trivial_ints {
-        ($($t:ident),*) => {
-            $(
-                #[test]
-                fn $t() {
-                    let v: $t = 42;
-
-                    let bytes = v.get_bytes();
-                    let mut v2: $t = 0;
-                    v2.from_bytes(&bytes);
-
-                    assert_eq!(v, v2);
-                    assert_eq!(bytes.len(), std::mem::size_of::<$t>());
-                }
-            )*
-        };
-    }
-    macro_rules! test_trivial_floats {
-        ($($t:ident),*) => {
-            $(
-                #[test]
-                fn $t() {
-                    let v: $t = 42.0;
-
-                    let bytes = v.get_bytes();
-                    let mut v2: $t = 0.0;
-                    v2.from_bytes(&bytes);
-
-                    assert_eq!(v, v2);
-                    assert_eq!(bytes.len(), std::mem::size_of::<$t>());
-                }
-            )*
-        };
-    }
-
-    test_trivial_ints!(i8, u8, i16, u16, i32, u32, i64, u64);
-    test_trivial_floats!(f32, f64);
-}
+mod tests;
